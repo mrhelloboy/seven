5ad37ab823c11899bb8ec55ea9b5e0d3064f9580
mrhelloboy <linzhanqi123@gmail.com>
2023-06-16 19:53:54 +0800

    fix: active status of toc when scroll or click

--- static/js/toc.js
+++ static/js/toc.js
@@ -3,15 +3,22 @@
  */
 
 const tocDiv = document.getElementById("TableOfContents");
-const liEleList = tocDiv.querySelectorAll("ul > li");
+const nodeListOnToc = tocDiv.querySelectorAll("ul > li");
 let currentLevel = 1;
 let allLevel = 1;
 let hasInitActive = false;
 let catalogText = [];
 let hadClick = false;
+let currentHeadNode = null;
+let currentHeadNodeToc = 0;
+let isBottom = false;
+let headNodeList = [];
 let lastActiveInScroll = null;
 
-liEleList.forEach((ele) => {
+let isVisual = false;  // 标题是否在窗口可视范围中
+let canScrollSmoot = true;
+
+nodeListOnToc.forEach((ele, idx) => {
   const aNode = ele.querySelector("a");
   aNode.classList.add("theme-toc");
 
@@ -29,14 +36,27 @@ liEleList.forEach((ele) => {
 
   // 每条目录添加点击事件
   aNode.addEventListener("click", (e) => {
+    // 移除所有active状态下的目录
     const allActiveNodes = document.querySelectorAll(".theme-toc-active");
     allActiveNodes.forEach((node) => {
       disableActive(node);
     });
+    // 当前目录设置为active
     onActive(aNode);
     
+    // 设置为已点击
     hadClick = true;
+    // active状态的目录节点
     lastActiveInScroll = aNode;
+    // 对应文章的标题节点
+    currentHeadNode = headNodeList[idx];
+
+    isVisual = isContain(currentHeadNode);
+    if (isBottom && isVisual) {
+      canScrollSmoot = false;
+    } else {
+      canScrollSmoot = true;
+    }
   });
 });
 
@@ -47,34 +67,64 @@ HEles.forEach((ele) => {
   const t = ele.innerText;
   let idx = catalogText.indexOf(t);
   if (idx > -1) {
+    headNodeList.push(ele);
     HElesMap.set(ele, idx);
   }
 });
 
 // 随页面滚动，激活目录选中状态
+let minTopOfHead = null;
+let minTop = Number.MAX_VALUE;
 document.addEventListener("scroll", function (e) {
-  if (hadClick) {
-    hadClick = false;
-    return
-  };
-  if (this.documentElement.getBoundingClientRect().top === 0 && lastActiveInScroll) {
-    disableActive(lastActiveInScroll);
-  }
+  
+  // 是否滚动到底部了
+  isBottom = (this.documentElement.scrollTop + this.documentElement.clientHeight) === this.documentElement.scrollHeight;
 
-  HElesMap.forEach((value, key, map) => {
-    let h = key.getBoundingClientRect().top;
+  // 点击目录，文章在滚动中
+  if (hadClick && currentHeadNode && canScrollSmoot) {
+    // 如何判断点击后，文章滚动已结束
+    // 标题已经到达窗口顶部
+    const newHeadNodeTop = currentHeadNode.getBoundingClientRect().top
+    if (newHeadNodeTop === 0) {
+      hadClick = false;
+      currentHeadNode = null;
+      canScrollSmoot = false;
+      return;
+    } 
+  } else {
 
-    if (h > 70 && h < 200) {
-      liCatalog = liEleList[value];
-      const node = liCatalog.querySelector("a");
-      if (lastActiveInScroll) {
-        disableActive(lastActiveInScroll);
-      }
-      onActive(node);
-      lastActiveInScroll = node;
+    if (this.documentElement.getBoundingClientRect().top === 0 && lastActiveInScroll) {
+      disableActive(lastActiveInScroll);
     }
-  });
+
+    minTop = Number.MAX_VALUE ;
+    HElesMap.forEach((value, key, map) => {
+      let h = key.getBoundingClientRect().top;
+      if (h >= 0 && h < minTop && isContain(key)) {
+        liCatalog = nodeListOnToc[value];
+        minTop = h;
+        minTopOfHead = liCatalog.querySelector("a");
+      } 
+    });
+
+    if (lastActiveInScroll) {
+      disableActive(lastActiveInScroll);
+    }
+    onActive(minTopOfHead);
+    lastActiveInScroll = minTopOfHead; 
+  }
 });
+
+
+function isContain(dom) {
+  const totalHeight = window.innerHeight || document.documentElement.clientHeight;
+  const totalWidth = window.innerWidth || document.documentElement.clientWidth;
+  // 当滚动条滚动时，top, left, bottom, right时刻会发生改变。
+  const { top, right, bottom, left } = dom.getBoundingClientRect();
+  // console.log(top, right, bottom, left)
+  return (top >= 0 && left >= 0 && right <= totalWidth && bottom <= totalHeight);
+}
+
 
 // 选中状态
 function onActive(node) {
