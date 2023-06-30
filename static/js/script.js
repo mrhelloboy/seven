3bc3f6f1c7e25e1a0a3f0fedd85cee3bab8397ef
mrhelloboy <linzhanqi123@gmail.com>
2023-06-30 22:56:51 +0800

    Optimized hamburger button and header and footer logo

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,21 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logoToggle = document.getElementById("logo-toggle");
+const logo = document.getElementById("logo");
 
 // Hamburger button listener
-btn.addEventListener('click', navToggle)
+btn.addEventListener("click", navToggle);
 
 function navToggle() {
-    btn.classList.toggle('open')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
-  
-    // if (menu.classList.contains('flex')) {
-    //   logo.setAttribute('src', './images/logo-bookmark-footer.svg')
-    // } else {
-    //   logo.setAttribute('src', './images/logo-bookmark.svg')
-    // }
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex") && logoToggle) {
+    logo.classList.add("hidden");
+    logoToggle.classList.remove("hidden");
+  } else {
+    logo.classList.remove("hidden");
+    logoToggle.classList.add("hidden");
+  }
 }
