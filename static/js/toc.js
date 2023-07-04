fb36890f99d733eea151b52d278a6f2078f36346
mrhelloboy <linzhanqi123@gmail.com>
2023-07-05 02:42:17 +0800

    Refactor the catalog

--- static/js/toc.js
+++ static/js/toc.js
@@ -2,132 +2,80 @@
  * 文章目录css样式、及状态（hover、active等）处理逻辑
  */
 
-const tocDiv = document.getElementById("TableOfContents");
-const nodeListOnToc = tocDiv.querySelectorAll("ul > li");
-let currentLevel = 1;
-let allLevel = 1;
-let hasInitActive = false;
-let catalogText = [];
-let hadClick = false;
-let currentHeadNode = null;
-let currentHeadNodeToc = 0;
-let isBottom = false;
-let headNodeList = [];
-let lastActiveInScroll = null;
+let tocMapHeadTag = new Map();  // 目录跟文章的标题对应关系
+let headTagMapToc = new Map();  // 文章标题跟目录的对应关系
 
-let isVisual = false;  // 标题是否在窗口可视范围中
-let canScrollSmoot = true;
+// 获取文章所有的heading元素
+const allHeads = document.querySelectorAll(".head-tag");
 
-nodeListOnToc.forEach((ele, idx) => {
-  const aNode = ele.querySelector("a");
-  aNode.classList.add("theme-toc");
+const startNodeLists = document.querySelectorAll("#TableOfContents > ul > li");
 
-  catalogText.push(aNode.innerText);
+// Add curstom css for ele of toc
+startNodeLists.forEach((node)=>{
+  findNextNodeLists(node, 0);
+})
 
-  // 多级目录样式编排
-  if (allLevel > currentLevel) {
-    aNode.classList.add("theme-toc-nest");
-    allLevel -= 1;
-  }
-  const childLength = ele.childNodes.length;
-  if (childLength > allLevel) {
-    allLevel += childLength;
+function findNextNodeLists(node, level) {
+  if (level >= 10) return;
+
+  const nextNodeLists = node.querySelectorAll("ul > li");
+
+  if (level === 0) {
+    hyperLinkNodeAddClass(node, "theme-toc");
+    hyperLinkNodeAddEvent(node);
+    findTocMapHeadTag(node);
+
+  } else if (nextNodeLists.length === 0 && level > 0) {
+    let paddingLeft = `theme-toc-nest-${level * 1}`
+    hyperLinkNodeAddClass(node, `theme-toc ${paddingLeft}`)
+    hyperLinkNodeAddEvent(node);
+    findTocMapHeadTag(node);
   }
 
-  // 每条目录添加点击事件
-  aNode.addEventListener("click", (e) => {
-    // 移除所有active状态下的目录
-    const allActiveNodes = document.querySelectorAll(".theme-toc-active");
-    allActiveNodes.forEach((node) => {
-      disableActive(node);
-    });
-    // 当前目录设置为active
-    onActive(aNode);
-    
-    // 设置为已点击
-    hadClick = true;
-    // active状态的目录节点
-    lastActiveInScroll = aNode;
-    // 对应文章的标题节点
-    currentHeadNode = headNodeList[idx];
+  level += 1;
+  nextNodeLists.forEach((nextNode)=>{
+    return findNextNodeLists(nextNode, level)}
+  )
 
-    isVisual = isContain(currentHeadNode);
-    if (isBottom && isVisual) {
-      canScrollSmoot = false;
-    } else {
-      canScrollSmoot = true;
-    }
-  });
-});
-
-// 文章的标题元素对应目录的索引
-const HEles = document.querySelectorAll("H1, H2, H3, H4, H5");
-let HElesMap = new Map();
-HEles.forEach((ele) => {
-  const t = ele.innerText;
-  let idx = catalogText.indexOf(t);
-  if (idx > -1) {
-    headNodeList.push(ele);
-    HElesMap.set(ele, idx);
-  }
-});
-
-// 随页面滚动，激活目录选中状态
-let minTopOfHead = null;
-let minTop = Number.MAX_VALUE;
-document.addEventListener("scroll", function (e) {
-  
-  // 是否滚动到底部了
-  isBottom = (this.documentElement.scrollTop + this.documentElement.clientHeight) === this.documentElement.scrollHeight;
-
-  // 点击目录，文章在滚动中
-  if (hadClick && currentHeadNode && canScrollSmoot) {
-    // 如何判断点击后，文章滚动已结束
-    // 标题已经到达窗口顶部
-    const newHeadNodeTop = currentHeadNode.getBoundingClientRect().top
-    if (newHeadNodeTop === 0) {
-      hadClick = false;
-      currentHeadNode = null;
-      canScrollSmoot = false;
-      return;
-    } 
-  } else {
-
-    if (this.documentElement.getBoundingClientRect().top === 0 && lastActiveInScroll) {
-      disableActive(lastActiveInScroll);
-    }
-
-    minTop = Number.MAX_VALUE ;
-    HElesMap.forEach((value, key, map) => {
-      let h = key.getBoundingClientRect().top;
-      if (h >= 0 && h < minTop && isContain(key)) {
-        liCatalog = nodeListOnToc[value];
-        minTop = h;
-        minTopOfHead = liCatalog.querySelector("a");
-      } 
-    });
-
-    if (lastActiveInScroll) {
-      disableActive(lastActiveInScroll);
-    }
-
-    if (minTopOfHead) {
-      onActive(minTopOfHead);
-      lastActiveInScroll = minTopOfHead; 
-    } 
-  }
-});
-
-
-function isContain(dom) {
-  const totalHeight = window.innerHeight || document.documentElement.clientHeight;
-  const totalWidth = window.innerWidth || document.documentElement.clientWidth;
-  // 当滚动条滚动时，top, left, bottom, right时刻会发生改变。
-  const { top, right, bottom, left } = dom.getBoundingClientRect();
-  // console.log(top, right, bottom, left)
-  return (top >= 0 && left >= 0 && right <= totalWidth && bottom <= totalHeight);
 }
 
+// Add class for <a>
+function hyperLinkNodeAddClass(node, classNames) {
+  const hyperLinkNode = node.querySelector("a");
+  classNames.split(" ").forEach((className)=>{
+    hyperLinkNode.classList.add(className);
+  })
+}
+
+function hyperLinkNodeAddEvent(node){
+  const hyperLinkNode = node.querySelector("a");
+  hyperLinkNode.href = "";
+  hyperLinkNode.addEventListener("click", tocOnClick);
+}
+
+function findTocMapHeadTag(node) {
+  const hyperLinkNode = node.querySelector("a");
+  allHeads.forEach((headNode) => {
+    if (headNode.innerText === hyperLinkNode.innerText) {
+      tocMapHeadTag.set(hyperLinkNode, headNode);
+      headTagMapToc.set(headNode, hyperLinkNode);
+    }
+  })
+}
+
+function tocOnClick(e) {
+  e.preventDefault();
+
+  // 移除所有active状态下的目录
+  removeAllActive();
+
+  // 当前目录设置为active
+  onActive(e.target);
+
+  const _headNode = tocMapHeadTag.get(e.target);
+  const headNode = document.querySelector(`#${_headNode.id}`)
+  window.scrollTo({top: headNode.offsetTop - 80, behavior:"smooth"});
+}
 
 // 选中状态
 function onActive(node) {
@@ -140,3 +88,52 @@ function disableActive(node) {
   node.classList.remove("theme-toc-active");
   node.classList.add("theme-toc");
 }
+
+// 移除所有选中状态的目录
+function removeAllActive() {
+  const allActiveNodes = document.querySelectorAll(".theme-toc-active");
+  allActiveNodes.forEach((node) => {
+    disableActive(node);
+  });
+}
+
+let intersectionEles = [];
+const intersectionObserver = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry =>{
+    console.log(entry)
+    if (entry.isIntersecting) {
+      intersectionEles.push(entry.target)
+      
+    }
+
+    if (!entry.isIntersecting) {
+      const idx = intersectionEles.indexOf(entry.target);
+      if (idx !== -1) {
+        intersectionEles.splice(idx, 1);
+      }
+    }
+  })
+
+  let minTop = Number.MAX_VALUE;
+  let targetEle = null;
+  if (intersectionEles.length > 0) {
+    intersectionEles.forEach(element => {
+      if (element.offsetTop <= minTop) {
+        minTop = element.offsetTop;
+        targetEle = element;
+      }
+    })
+  }
+
+  if (targetEle && headTagMapToc.get(targetEle)) {
+    removeAllActive();
+    onActive(headTagMapToc.get(targetEle));
+  }
+
+});
+
+// 开始监听
+allHeads.forEach((head)=>{
+  intersectionObserver.observe(head);
+})
