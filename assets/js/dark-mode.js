eac90aa6e603b3b49d1529138c07c582270e6bf1
mrhelloboy <linzhanqi123@gmail.com>
2023-12-19 13:25:43 +0800

    将dark mode的代码独立开,及logo添加dark模式处理

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,60 @@
+let themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+let themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// logo icon
+let logoToggleDarkIcon = document.getElementById("logo-dark-icon");
+let logoToggleLightIcon = document.getElementById("logo-light-icon");
+
+// footer theme logo icon
+let footerToggleDarkIcon = document.getElementById("footer-theme-dark-logo");
+let footerToggleLightIcon = document.getElementById("footer-theme-light-logo");
+
+// Change the icons inside the button based on previous settings
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+  logoToggleDarkIcon.classList.remove("hidden");
+  footerToggleDarkIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+  logoToggleLightIcon.classList.remove("hidden");
+  footerToggleLightIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  logoToggleDarkIcon.classList.toggle("hidden");
+  logoToggleLightIcon.classList.toggle("hidden");
+
+  footerToggleDarkIcon.classList.toggle("hidden");
+  footerToggleLightIcon.classList.toggle("hidden");
+
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
