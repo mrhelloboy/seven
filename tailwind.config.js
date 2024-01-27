854ccad38c1ccbaf5e536b1be2dd11cfcdf8bc1e
mrhelloboy <linzhanqi123@gmail.com>
2024-01-28 01:44:08 +0800

    Remove old css

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,32 +27,6 @@ module.exports = {
         22: '5.5rem',
         38: '9.5rem',
       },
-      animation: {
-        zoomIn: 'zoomIn .8s ease-out 1',
-        zoomOut: 'zoomOut 1s ease-in-out 1',
-      },
-      keyframes: {
-        zoomIn: {
-          from: {
-            transform: 'scale(0)',
-            opacity: 0,
-          },
-          to: {
-            transform: 'scale(1)',
-            opacity: 1,
-          },
-        },
-        zoomOut: {
-          from: {
-            transform: 'scale(1)',
-            opacity: 1,
-          },
-          to: {
-            transform: 'scale(0)',
-            opacity: 0,
-          },
-        },
-      },
     },
   },
   plugins: [require('flowbite-typography')],
